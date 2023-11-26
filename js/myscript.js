fb2e2a6195a5a212f555fc7acc64a7dd3ef4502d
elisabichiappini <elisabichiappini@gmail.com>
2023-11-26 22:12:32 +0100

    feat: event and function in vue

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,23 @@
 /*                          javascript                      */
-const h1Element = document.querySelector('h1');
-const inputElement = document.querySelector('input');
 
-inputElement.addEventListener('input', function(event) {
-    console.log('sto ascoltando questo evento');
-    // console.log(event.target.value);
-    console.log(this.value);
-    //  qui dico all'elementoh1 di sovrascriversi il valore che inserisco nell'input
-    h1Element.innerHTML = this.value;
-})
+// codice javascript di vue
+const { createApp } = Vue
+    createApp({
+        data() {
+        return {
+            className: 'big',
+            title: 'Elisa'
+            }
+        },
+        methods: {
+            greetings() {
+                return 'ciao'
+            },
+            sayHelloViaConsole () {
+                console.log(this.title)
+            }
+        }
+    
+  }).mount('#app')
+
+  
